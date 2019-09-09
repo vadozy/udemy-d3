099159fa9fa3d3e5f855a6cf035d8d9efb478569
Vadim Storozhuk <vadim.storo@gmail.com>
2019-09-08 23:10:27 -0400

    2.05.0

--- 02/2.05.0/js/main.js
+++ 02/2.05.0/js/main.js
@@ -3,3 +3,14 @@
 *    Mastering Data Visualization with D3.js
 *    2.5 - Activity: Adding SVGs to the screen
 */
+
+d3.select('#chart-area')
+  .append('svg')
+  .attr('width', 800)
+  .attr('height', 600)
+    .append('rect')
+    .attr('x', 50)
+    .attr('y', 10)
+    .attr('width', 100)
+    .attr('height', 200)
+    .attr('fill', '#0ff')
