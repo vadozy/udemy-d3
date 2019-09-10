a2f43aef0e0abf227e9e3d79553cb7abf4a34c1e
Vadim Storozhuk <vadim.storo@gmail.com>
2019-09-09 22:47:04 -0400

    2.08.0

--- 02/2.08.0/js/main.js
+++ 02/2.08.0/js/main.js
@@ -3,3 +3,32 @@
 *    Mastering Data Visualization with D3.js
 *    2.8 - Activity: Your first visualization!
 */
+
+const RECT_WIDTH = 50;
+const GAP_BETWEEN_RECTS = 20;
+
+d3.json("data/buildings.json")
+  .then(data => {
+    data.forEach(v => {
+      v.height = +v.height;
+    });
+
+    const svg = d3.select("#chart-area")
+      .append("svg")
+        .attr("width", 500)
+        .attr("height", 500);
+
+    const rects = svg.selectAll("rect")
+      .data(data);
+
+    rects.enter()
+      .append("rect")
+        .attr("x", (_, i) => i * (RECT_WIDTH + GAP_BETWEEN_RECTS))
+        .attr("y", 10)
+        .attr("width", RECT_WIDTH)
+        .attr("height", d => d.height)
+        .attr("fill", "#288");
+
+    console.log(data);
+  })
+  .catch(console.log);
